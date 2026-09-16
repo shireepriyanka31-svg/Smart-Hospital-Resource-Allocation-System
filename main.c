dbcedaf7e9cc9259894c874d16eb24eceef38507
shireepriyanka31-svg <shireepriyanka31@gmail.com>
2026-09-16 13:21:59 +0530

    Main Menu.c
    Add Main menu

--- main.c
+++ main.c
@@ -3,6 +3,18 @@
 
 int main()
 {
-    printf("Hello world!\n");
+    int choice;
+    do {
+        printf("\n============ SMART HOSPITAL SYSTEM MENU ============\n");
+        printf("1. Register New Patient\n");
+        printf("2. Display Bed Occupancy\n");
+        printf("3. Display Patients by Priority (Triage)\n");
+        printf("4. Generate Summary Report\n");
+        printf("5. Save & Exit\n");
+        printf("======================================================\n");
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
+    }
+    while (choice != 5);
     return 0;
 }
